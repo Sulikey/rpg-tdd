de60b729e37bcc78cd0df90d768c067f1c8cd0ba
Sulikey <anahi.sulikey@gmail.com>
2022-11-10 14:45:40 -0800

    Add fighting and cards
    Co-authored-by: Robert Bryan <robbryandev@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ import game from "./json/game.json";
 import Wizard from "./js/wizard.js";
 
 let gameLoop;
+let startButton;
+
+function fight() {
+  if (game.turn) {
+    game.players[2].rollDamage();
+    game.players[1].damage(game.players[2].roll);
+    game.turn = 0;
+  } else {
+    game.players[1].rollDamage();
+    game.players[2].damage(game.players[1].roll);
+    game.turn = 1;
+  }
+}
 
 function setValues() {
   let wizOneName = document.getElementById("wiz-0-name");
@@ -18,22 +31,36 @@ function setValues() {
 
   let wizTwoHp = document.getElementById("wiz-1-health");
   wizTwoHp.innerText = game.players[2].health;
+
+  game.players.forEach((wiz) => {
+    if (wiz.health !== undefined) {
+      if (wiz.health === 0) {
+        game.started = false;
+        document.getElementById("hit").classList.add("invisible");
+        document.querySelectorAll(".wiz-card").forEach((wizCard) => {
+          wizCard.classList.add("invisible");
+        });
+        document.getElementById("start-spot").appendChild(startButton);
+      }
+    }
+  });
 }
 
 function displayWizard() {
   setValues();
   gameLoop = setInterval(() => {
     setValues();
-  }, 2000);
+  }, 400);
 
   document.querySelectorAll(".wiz-card").forEach((wiz) => {
     wiz.classList.remove("invisible");
   });
   document.getElementById("hit").classList.remove("invisible");
+  document.getElementById("hit").addEventListener("click", fight);
 }
 
-function startGame() {
-  game.started = true;
+function initPlayers() {
+  game.players = [];
   let players = Array(game.players);
 
   const wizardOne = new Wizard("hermione", 100);
@@ -41,6 +68,11 @@ function startGame() {
   const wizardTwo = new Wizard("voldemort", 90);
   players.push(wizardTwo);
   game.players = players;
+}
+
+function startGame() {
+  game.started = true;
+  initPlayers();
 
   displayWizard(0);
   displayWizard(1);
@@ -48,6 +80,7 @@ function startGame() {
 
 function handleStart() {
   startGame();
+  startButton = this;
   this.parentElement.removeChild(this);
 }
 
